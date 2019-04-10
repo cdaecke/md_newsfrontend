e1d7b71da6d8a62f42cdcd39b7380fe530470df2
chris <typo3@mediadreams.org>
2019-04-10 17:28:52 +0200

    [TASK] load news model of this extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function()
     {
+        $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'md_newsfrontend';
 
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
             'Mediadreams.MdNewsfrontend',
