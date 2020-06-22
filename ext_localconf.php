2619ffec224993f7b268a89d5d34d8036c8df02e
chris <typo3@mediadreams.org>
2020-06-22 23:46:57 +0200

    [TASK] use controller classes when registering plugins/modules

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,17 +4,16 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function()
     {
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'md_newsfrontend';
-
+        
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'Mediadreams.MdNewsfrontend',
+            'MdNewsfrontend',
             'Newsfe',
             [
-                'News' => 'list, new, create, edit, update, delete'
+                \Mediadreams\MdNewsfrontend\Controller\NewsController::class => 'list, new, create, edit, update, delete'
             ],
             // non-cacheable actions
             [
-                'News' => 'list, create, update, delete'
+                \Mediadreams\MdNewsfrontend\Controller\NewsController::class => 'list, create, update, delete'
             ]
         );
         
