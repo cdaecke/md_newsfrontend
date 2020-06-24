65e388a7bfd660eb8a277b3bc6c707b45655ff81
chris <typo3@mediadreams.org>
2020-06-24 21:35:17 +0200

    [TASK] raise version number to 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,11 +20,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.5',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'news' => '7.0.0-7.99.99',
+            'typo3' => '9.5.0-10.4.99',
+            'news' => '7.0.0-8.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
