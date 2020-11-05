a2cbce914a0ee83949ea8eb26afdd0c84f7f6bfd
chris <typo3@mediadreams.org>
2020-11-05 12:12:40 +0100

    [BUGFIX] add proxy-class generation for ext:news again

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,12 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function()
     {
+
+        /**
+         * Extend ext:news
+         */
+        $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'md_newsfrontend';
+
         
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
             'Mediadreams.MdNewsfrontend',
