34ccd13932729c26557dd8812d3360cf338acc8f
chris <typo3@mediadreams.org>
2025-04-10 16:55:19 +0200

    [TASK] update plugin configuration

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'tx_mdnewsfrontend_newsfe' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:md_newsfrontend/Resources/Public/Icons/plugin_newsfe.svg'
+    ],
+];
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,7 +13,10 @@ call_user_func(
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'MdNewsfrontend',
             'Newsfe',
-            'News frontend'
+            'LLL:EXT:md_newsfrontend/Resources/Private/Language/locallang_db.xlf:tx_md_newsfrontend_newsfe.name',
+            'tx_mdnewsfrontend_newsfe',
+            null,
+            'LLL:EXT:md_newsfrontend/Resources/Private/Language/locallang_db.xlf:tx_md_newsfrontend_newsfe.description',
         );
 
     }
