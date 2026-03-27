05e7723288168739a47da4c4f085a93ae6df6314
chris <typo3@mediadreams.org>
2026-03-27 13:33:15 +0100

    [!!!][TASK] update dependency to TYPO3 v14. Drop support for v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '13.4.0-14.4.99',
             'news' => '11.0.0-14.99.99',
         ],
         'conflicts' => [],
